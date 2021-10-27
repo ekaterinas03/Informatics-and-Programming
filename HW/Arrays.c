7af24d51c4157ac7b9de12237b2f9fa7105032e5
Екатерина Барсукова <90093397+ekaterinas03@users.noreply.github.com>
2021-10-27 09:02:13 +0300

    работа 08.10 и Task3

---
+++ HW/Arrays.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+# define M_PI		3.14159265358979323846	/* pi */
+
+
+int main() {
+	char* locale = setlocale(LC_ALL, ""); //установим локализацию символов в консоли
+	int arr[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int summ = 0;
+
+	for (int i = 0; i < sizeof(arr) / 4; i++) { //сумма элементов с четным занчением
+		if (arr[i] % 2 == 0) {
+			summ += arr[i];
+		}
+	}
+	printf("%i \n", summ);
+
+
+	summ = 0;
+
+
+	for (int i = 0; i < sizeof(arr) / 4; i += 2) {//сумма элементов с четным индексом
+		summ = summ + arr[i];
+	}
+	printf("%i \n", summ);
+
+
+	for (int i = 0; i < sizeof(arr) / 4; i++) {//Исходный массив
+		printf("%i ", arr[i]);
+	}
+	printf("\nВведите индекс нужного элемента ");
+	int zamenaIndex;
+	scanf("%i", &zamenaIndex);
+	printf("Введите значение ");
+	scanf("%i", &arr[zamenaIndex]);
+	for (int i = 0; i < sizeof(arr) / 4; i++) {//Измененный массив
+		printf("%i ", arr[i]);
+	}
+	printf("\n \n");
+
+	//развернуть массив
+	int arr1[] = { 0,1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < sizeof(arr1) / 4; i++) {//Исходный массив
+		printf("%i ", arr1[i]);
+	}
+	//Без дополнительного массива
+	int leight = sizeof(arr1) / 4;
+	for (int i = 0; i < leight / 4; i++) {
+		int temp = arr1[i];
+		arr1[i] = arr1[leight - 1 - i];
+		arr1[leight - 1 - i] = temp;
+	}
+	printf("\n");
+	for (int i = 0; i < sizeof(arr1) / 4; i++) {//Реверсированный массив
+		printf("%i ", arr1[i]);
+	}
+	//С дополнительным массивом
+	int arr1Copy[10];
+	for (int i = 0; i < sizeof(arr1) / 4; i++) {
+		arr1Copy[i] = arr1[leight - 1 - i];
+	}
+	printf("\n");
+	for (int i = 0; i < sizeof(arr1Copy) / 4; i++) {//Реверсированный массив
+		printf("%i ", arr1Copy[i]);
+	}
+}
+return 0;
---
+++ HW/Cows.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+void generated(char *a, int n) {
+	srand(time(NULL));
+	for (int i = 0;i < n;i++) {
+		a[i] = 1 + rand() % 8;
+	}
+	for (int i = 0;i < n;i++) {
+		for (int j = 0;j < n;j++) {
+			if (a[i] == a[j] && i != j) { a[j] += 1; }
+		}
+		printf("%d", a[i]);
+		
+	}
+	printf("\n");
+}
+
+int main() {
+	char* locale = setlocale(LC_ALL, ""); //установим локализацию символов в консоли
+	int n;
+	int cows = 0, bull = 0;
+	scanf("%d", &n);
+
+
+	char* CPU = NULL;
+	CPU = (int*)malloc(n * sizeof(int));
+	generated(CPU, n);
+
+	while (bull != n) {
+		int pers;
+		cows = 0; bull = 0;
+		scanf("%d", &pers);
+		char* persArr = NULL;
+		persArr = (int*)malloc(n * sizeof(int));
+		for (int i = n - 1; pers; pers /= 10) persArr[i--] = pers % 10;
+		for (int i = 0;i < n;i++) {
+			int c = persArr[i];
+			for (int j = 0; j < n;j++) {
+				if (c == CPU[j] && i == j) {
+					bull++;
+				}
+				else if (c == CPU[j] && i != j) {
+					cows++;
+				}
+			}
+		}
+		printf("Результат: %d cows и %d bulls \n",cows,bull);
+	}
+}
